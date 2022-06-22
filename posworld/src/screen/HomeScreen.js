14794d1dbcd0b339928a501080fd077faecfcf5c
hyunmin <qweasx10@hanmail.net>
2022-06-22 17:52:18 +0900

    sad

--- posworld/src/screen/HomeScreen.js
+++ posworld/src/screen/HomeScreen.js
@@ -1,7 +1,12 @@
-import { View } from "react-native";
+import { StyleSheet, View } from 'react-native';
 
 function HomeScreen() {
-  return <View></View>;
+    return <View style={styles.headerStyle}></View>;
 }
-
+const styles = StyleSheet.create({
+    headerStyle: {
+        flex: 1,
+        backgroundColor: '#29b6f6',
+    },
+});
 export default HomeScreen;

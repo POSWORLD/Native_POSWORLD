92c29c991ba00a0151af6ced5127356b5fa0bd83
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-23 10:26:28 +0900

    [fix] master

--- posworld/App.js
+++ posworld/App.js
@@ -3,23 +3,23 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./src/screen/LoginScreen";
 import BottomNavigation from "./src/screen/navigation/BottomNavigation";
-
+import IndexNavigate from "./src/screen/navigation/IndexNavigate";
 
 export default function App() {
-   return (
-      <SafeAreaProvider>
-         <SafeAreaView style={{ flex: 1 }}>
-            <IndexNavigate></IndexNavigate>
-         </SafeAreaView>
-      </SafeAreaProvider>
-   );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <IndexNavigate></IndexNavigate>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });

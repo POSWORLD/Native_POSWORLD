b4e405030baca30ecd642560dbeedbd3591ec7dd
HjuH <hjuhee9807@naver.com>
2022-06-24 15:53:55 +0900

    pull main && photo insert, delete

--- posworld/src/store/photosApi.js
+++ posworld/src/store/photosApi.js
@@ -1,22 +1,40 @@
-import { customAxios } from "../http/CustomAxios";
-import { call, put } from "redux-saga/effects";
-import { SELECT_PHOTO_FAIL, SELECT_PHOTO_SUCCESS } from "./photos";
+import { customAxios } from '../http/CustomAxios';
+import { call, put } from 'redux-saga/effects';
+import { SELECT_PHOTO_FAIL, SELECT_PHOTO_SUCCESS } from './photos';
 
 export const getPhoto = function* (action) {
-  try {
-    const result = yield call(getPhotoApi, action.id);
-    yield put({ type: SELECT_PHOTO_SUCCESS, data: result });
-  } catch (err) {
-    yield put({ type: SELECT_PHOTO_FAIL, data: err });
-  }
+    try {
+        const result = yield call(getPhotoApi, action.id);
+        yield put({ type: SELECT_PHOTO_SUCCESS, data: result });
+    } catch (err) {
+        yield put({ type: SELECT_PHOTO_FAIL, data: err });
+    }
 };
 
 export const getPhotoApi = async (id) => {
-  try {
-    const response = await customAxios(`photo/${id}`, "get");
+    try {
+        const response = await customAxios(`photo/${id}`, 'get');
 
-    return response;
-  } catch (error) {
-    throw error;
-  }
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const postPhotoApi = async (photo) => {
+    try {
+        const response = await customAxios('photo/2', 'post', photo);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deletePhotoApi = async (id) => {
+    try {
+        const response = await customAxios(`photo/${Number(id)}/2`, 'get');
+        return response;
+    } catch (error) {
+        throw error;
+    }
 };

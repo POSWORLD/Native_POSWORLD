a5dd51a997eca4bc69b61d6408631874aa9e1775
HjuH <43878096+HjuH@users.noreply.github.com>
2022-06-24 16:35:58 +0900

    Revert "[Add] board delete success"

--- posworld/src/store/boardsApi.js
+++ posworld/src/store/boardsApi.js
@@ -1,8 +1,10 @@
 import { customAxios } from '../http/CustomAxios';
 
 export const insertBoard = async board => {
+   console.log('myId', board);
    try {
       const response = await customAxios(`/board/${board.homeId}/${board.userId}`, 'post', board);
+      console.log(response);
       return response;
    } catch (error) {
       throw error;
@@ -13,20 +15,6 @@ export const getBoardByHomeId = async homeId => {
    try {
       const response = await customAxios(`/board/${homeId}`, 'get');
       return response;
-   } catch (error) {
-      throw error;
-   }
-};
-
-export const deleteBoardByNum = async (num, board) => {
-   try {
-      const boardnum = Number(num);
-      const response = await customAxios(`/board/${boardnum}`, 'delete');
-
-      if (response.deleted == true) {
-         const delBoards = await board.filter(board => board.num !== num);
-         return delBoards;
-      }
    } catch (error) {
       throw error;
    }

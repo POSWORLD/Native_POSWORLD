29b91129048b7ce463e60c1246f2c1afb2f48b8c
hsb312 <hsb973@naver.com>
2022-06-24 15:47:40 +0900

    useSelect(state.user.me)

--- posworld/src/store/user.js
+++ posworld/src/store/user.js
@@ -1,24 +1,28 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { takeLatest } from "redux-saga/effects";
 import produce from "immer";
-import { loginCheckValue, loginValue } from "./usersApi";
+import { loginCheckValue, loginValue, updateUserValue } from "./usersApi";
 import {
   INIT,
   LOGIN_SUCCESS,
   LOGIN,
   LOGIN_CHECK,
   LOGIN_CHECK_SUCCESS,
+  UPDATE_USER,
+  UPDATE_USER_SUCCESS,
 } from "./actionType";
 
 //액션 함수
 export const init = () => ({ type: INIT });
 export const login = (params) => ({ type: LOGIN, params });
 export const loginCheck = (params) => ({ type: LOGIN_CHECK, params });
+export const updateUser = (params) => ({ type: UPDATE_USER, params });
 
 //사가함수
 export function* UserSaga() {
   yield takeLatest(LOGIN, loginValue);
   yield takeLatest(LOGIN_CHECK, loginCheckValue);
+  yield takeLatest(UPDATE_USER, updateUserValue);
 }
 
 //초기상태
@@ -28,7 +32,6 @@ const initialUser = {
   success: false,
   enableAccess: false, //
   isLogin: false,
-  myToken: "",
   me: {},
   other: {},
 };
@@ -43,15 +46,18 @@ const user = (state = initialUser, action) =>
         draft.data = new FormData();
         break;
       case LOGIN_SUCCESS:
-        console.log("p(login)들어옴");
+        console.log("로그인 Success");
         const token = action.data.accessToken;
         AsyncStorage.setItem("token", token);
-        draft.myToken = action.data.accessToken;
         draft.isLogin = true;
         break;
       case LOGIN_CHECK_SUCCESS:
-        console.log("p(logincheck)들어옴");
-        console.log(action.data);
+        console.log("로그인체크 Success");
+        // console.log(action.data);
+        draft.me = action.data;
+        break;
+      case UPDATE_USER_SUCCESS:
+        console.log("유저업데이트 Success");
       default:
         return state;
     }

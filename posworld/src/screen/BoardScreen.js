51f5bbfc9c372429d16b106548329cc7f00a908d
wjddls1542 <wjddls1542@scnu.ac.kr>
2022-06-26 20:06:39 +0900

    finish

--- posworld/src/screen/BoardScreen.js
+++ posworld/src/screen/BoardScreen.js
@@ -1,6 +1,6 @@
 import { ActivityIndicator, Button, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import HeaderScreen from './HeaderScreen';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AntDesign } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,7 +8,6 @@ import { useEffect, useState } from 'react';
 import { deleteboard, selectboard } from '../store/boards';
 import { useIsFocused } from '@react-navigation/native';
 import BoardListScreen from './BoardListScreen';
-const myId = 1;
 
 const BoardScreen = ({ navigation }) => {
    const [boards, setBoards] = useState({
@@ -17,9 +16,26 @@ const BoardScreen = ({ navigation }) => {
       homeId: 0,
    }); /* useSelector((state)=>state.boards.board) */
    const dispatch = useDispatch();
+
+   const getHomeId = async () => {
+      return await AsyncStorage.getItem('homeId');
+   };
+
+   const getId = async () => {
+      return await AsyncStorage.getItem('myId');
+   };
+
    const isFocused = useIsFocused();
-   const getBoard = () => {
-      dispatch(selectboard(boards.homeId));
+
+   const getBoard = async () => {
+      const homeId = await getHomeId();
+      console.log(homeId);
+      if (homeId == undefined) {
+         const myId = await getId();
+         dispatch(selectboard(myId));
+      } else {
+         dispatch(selectboard(homeId));
+      }
    };
    const boardlist = useSelector(state => state.boards);
    //console.log('bod', boardlist);
@@ -27,28 +43,31 @@ const BoardScreen = ({ navigation }) => {
       getBoard();
    }, [isFocused]);
 
-   const onDel = () => {
-      alert('등록');
-      dispatch(deleteboard(num));
-   };
    return boardlist.loading ? (
-      <View>
-         <HeaderScreen name="방명록"></HeaderScreen>
+      <View style={{ flex: 1 }}>
+         <HeaderScreen style={{ flex: 1 }} name="방명록"></HeaderScreen>
          <ActivityIndicator />
       </View>
    ) : (
       <View>
-         <HeaderScreen name="방명록"></HeaderScreen>
+         <HeaderScreen style={{ flex: 1 }} name="방명록"></HeaderScreen>
          <View style={styles.container}>
-            <View style={styles.boardlist}>
+            <View style={{ flex: 2 }}>
                <FlatList
                   data={Object.keys(boardlist.board)} //
                   renderItem={key => <BoardListScreen boarditem={boardlist.board[key.index]} boardlist={boardlist} />}
                   keyExtractor={key => key}
                />
             </View>
-            <View style={styles.buttonStyle}>
-               <AntDesign name="form" size={50} color="black" onPress={() => navigation.navigate('BoardAdd')} />
+            <View style={{ flex: 1 }}>
+               <AntDesign
+                  position="right"
+                  style={styles.buttonStyle}
+                  name="form"
+                  size={50}
+                  color="black"
+                  onPress={() => navigation.navigate('BoardAdd')}
+               />
                {/* <AntDesign name="form" size={50} color="black" /> */}
             </View>
          </View>
@@ -78,9 +97,10 @@ const styles = StyleSheet.create({
    },
    buttonStyle: {
       position: 'absolute',
+      margin: 25,
       padding: 10,
-      borderRadius: 30,
-      right: 25,
+      right: 0,
+      flex: 1,
    },
    containers: {
       flex: 1,
